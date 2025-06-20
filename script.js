9b485fca4f30ef12bf9033e21959fdc38e13774b
tomazaquino163 <tomaz.aquino@escola.pr.gov.br>
2025-06-20 00:59:27 -0300

    implementação javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(){
+// criando as variáveis para receber os valores dos campos
+const loginInput = document.getElementById("login");
+const senhaInput = document.getElementById("senha");
+const enviarBtn = document.getElementById("enviar");
+// criando a função para fazer o botão funcionar
+enviarBtn.addEventListener('click', function(event){
+event.preventDefault();
+// criando mais duas variáveis para receber os valores fintrados (sem espaços)
+const login = loginInput.value.trim();
+const senha = senhaInput.value.trim();
+// verifica se os campos login e senha estão vazios
+if (login === "" || senha ===""){
+alert("Por favor, preencha todos os campos.");
+return;
+}
+// simulação de autenticação
+if(login === "admin" && senha === "1234"){
+alert("Login bem sucedido!");
+} else {
+alert("Login ou senha incorretos!");
+}
+});
+});
