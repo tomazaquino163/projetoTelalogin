92a087080356be5eca4c5c8268115575824ddd37
tomazaquino163 <tomaz.aquino@escola.pr.gov.br>
2025-06-20 01:02:54 -0300

    ESPAÇO

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
+
 // criando as variáveis para receber os valores dos campos
 const loginInput = document.getElementById("login");
 const senhaInput = document.getElementById("senha");
 const enviarBtn = document.getElementById("enviar");
+
 // criando a função para fazer o botão funcionar
 enviarBtn.addEventListener('click', function(event){
 event.preventDefault();
+
 // criando mais duas variáveis para receber os valores fintrados (sem espaços)
 const login = loginInput.value.trim();
 const senha = senhaInput.value.trim();
@@ -14,6 +17,7 @@ if (login === "" || senha ===""){
 alert("Por favor, preencha todos os campos.");
 return;
 }
+
 // simulação de autenticação
 if(login === "admin" && senha === "1234"){
 alert("Login bem sucedido!");
